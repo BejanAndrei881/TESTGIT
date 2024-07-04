3a0ae9a1a982fd0b2ae9e449fda7379a9d99d05e
Jhon <andreibejan16a@gmail.com>
2024-07-04 12:40:29 +0300

    homework

--- algorithms05.py
+++ algorithms05.py
@@ -8,16 +8,27 @@
 #     print(suma)
 # except ValueError:
 #     print("Numarul introdus nu este un intreg")
-try:
-    n = int(input("Introduceti un numar pentru a calcula numerele pare: "))
+# try:
+#     n = int(input("Introduceti un numar pentru a calcula numerele pare: "))
 
-    i = 1
-    suma = 0
-    while i <= n:
-        if i % 2 == 0:
-            suma = suma + i
-        i = i + 1
-    print(suma)
-except ValueError:
-    print("Numarul nu este integer")
+#     i = 1
+#     suma = 0
+#     while i <= n:
+#         if i % 2 == 0:
+#             suma = suma + i
+#         i = i + 1
+#     print(suma)
+# except ValueError:
+#     print("Numarul nu este integer")
     
+lista_mea = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+lista_mea.sort()
+print(lista_mea)
+# lista_mea = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+# list_pare=[]
+# for x in lista_mea:
+#     if x % 2 == 0:
+#         list_pare.append(x)
+# list_pare.sort()
+# print(list_pare)
+print(lista_mea[2::3])
